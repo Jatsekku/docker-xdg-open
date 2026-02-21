e660fdb12be810fd78214ba8d504553ddd62fbea
Jatsekku <jatsekku2@gmail.com>
2026-02-21 22:48:01 +0100

    sq to paackage

--- nix/package.nix
+++ nix/package.nix
@@ -1,23 +1,27 @@
-{ stdenv, cmake, glibc }:
+{
+  stdenv,
+  cmake,
+  glibc,
+}:
 stdenv.mkDerivation {
-    pname = "docker-xdg-open";
-    version = "0.1.0";
+  pname = "docker-xdg-open";
+  version = "0.1.0";
 
-    # Point to project root
-    src = ../.;
+  # Point to project root
+  src = ../.;
 
-    nativeBuildInputs = [
-        cmake
-        glibc
-        glibc.static
-    ];
+  nativeBuildInputs = [
+    cmake
+    glibc
+    glibc.static
+  ];
 
-    cmakeFlags = [
-        "-DBUILD_STATIC_CLIENT=OFF"
-        "-DBUILD_STATIC_SERVER=OFF"
-    ];
+  cmakeFlags = [
+    "-DBUILD_STATIC_CLIENT=OFF"
+    "-DBUILD_STATIC_SERVER=OFF"
+  ];
 
-    installPhase = ''
-        cmake --install . --prefix $out --component server
-    '';
+  installPhase = ''
+    cmake --install . --prefix $out --component server
+  '';
 }
